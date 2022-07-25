3c49297ffc81cadb5e7f8cb6ce9c553c925d0e4d
yenyu <diaoemily0830@gmail.com>
2022-07-26 02:20:27 +0800

    會員頁scss重構

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
 import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 // import 路由
 import Home from "./pages/Home";
 import Act from "./pages/Act";
 import Booking from "./pages/Booking";
-import Member from "./pages/Member";
+import Member from "./pages/Member/Member";
 import Recipes from "./pages/Recipes";
 import SuMap from "./pages/SuMap";
-import Login from "./pages/Login";
+import Login from "./pages/Login/";
 import NoFound from "./pages/noFound";
 
 function App() {
@@ -29,7 +30,11 @@ function App() {
                     <Route index element={<Home auth={auth} />} />
                     <Route path="Act" element={<Act auth={auth} />} />
                     <Route path="Booking" element={<Booking auth={auth} />} />
-                    <Route path="Member" element={<Member auth={auth} />} />
+                    <Route path="Member">
+                        <Route index element={<Member auth={auth} />} />
+                        {/* <Route path="Coupon" element={<Coupon />} />
+                        <Route path="Keep" element={<Keep />} /> */}
+                    </Route>
                     <Route path="Recipes" element={<Recipes auth={auth} />} />
                     <Route path="SuMap" element={<SuMap auth={auth} />} />
                     <Route
