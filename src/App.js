11bd3dbca729818629ce9989a984c6331d57cbfe
Jerry Wwang <doghost111@gmail.com>
2022-07-26 09:34:03 +0800

    App update

--- src/App.js
+++ src/App.js
@@ -10,37 +10,53 @@ import Recipes from "./pages/Recipes";
 import SuMap from "./pages/SuMap";
 import Login from "./pages/Login";
 import NoFound from "./pages/noFound";
+import Create from "./pages/Booking/sub-pages/Create";
+import Cart from "./pages/Carts";
+import Navbar from "./components/Navbar";
 
 function App() {
     const [auth, setAuth] = useState(false);
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route
-                    path="/"
-                    element={
-                        <>
-                            <h1>Welcome Home</h1>{" "}
-                            <p>please insert suyouong in your URL, thanks.</p>
-                        </>
-                    }
-                />
-                <Route path="/shuyoung">
-                    <Route index element={<Home auth={auth} />} />
-                    <Route path="Act" element={<Act auth={auth} />} />
-                    <Route path="Booking" element={<Booking auth={auth} />} />
-                    <Route path="Member" element={<Member auth={auth} />} />
-                    <Route path="Recipes" element={<Recipes auth={auth} />} />
-                    <Route path="SuMap" element={<SuMap auth={auth} />} />
+        <>
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
                     <Route
-                        path="Login"
-                        element={<Login auth={auth} setAuth={setAuth} />}
+                        path="/"
+                        element={
+                            <>
+                                <h1>Welcome Home</h1>{" "}
+                                <p>
+                                    please insert suyouong in your URL, thanks.
+                                </p>
+                            </>
+                        }
                     />
-                    {/* 404未找到的頁面路由，需放在最下方 */}
-                    <Route path="*" element={<NoFound />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
+                    <Route path="/shuyoung">
+                        <Route index element={<Home auth={auth} />} />
+                        <Route path="Act" element={<Act auth={auth} />} />
+                        <Route path="Booking">
+                            <Route index element={<Booking auth={auth} />} />
+                            <Route path="Create" element={<Create />} />
+                        </Route>
+
+                        <Route path="Member" element={<Member auth={auth} />} />
+                        <Route
+                            path="Recipes"
+                            element={<Recipes auth={auth} />}
+                        />
+                        <Route path="SuMap" element={<SuMap auth={auth} />} />
+                        <Route path="Cart" element={<Cart auth={auth} />} />
+                        <Route
+                            path="Login"
+                            element={<Login auth={auth} setAuth={setAuth} />}
+                        />
+                        {/* 404未找到的頁面路由，需放在最下方 */}
+                        <Route path="*" element={<NoFound />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </>
     );
 }
 
