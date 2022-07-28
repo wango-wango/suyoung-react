198809cc18c2bd6837016d8473ce66f05fc089d4
anmo <anmo0514@gmail.com>
2022-07-28 14:36:24 +0800

    增加活動路由

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import 路由
 import Home from "./pages/Home";
 import Act from "./pages/Act";
+import Atv from "./pages/Act/sub-pages/Atv";
+import Float from "./pages/Act/sub-pages/Float";
+import Night from "./pages/Act/sub-pages/Night";
+import Upstream from "./pages/Act/sub-pages/Upstream";
 import Booking from "./pages/Booking";
 import Member from "./pages/Member/Member";
 import Recipes from "./pages/Recipes";
@@ -38,10 +42,10 @@ function App() {
                         <Route index element={<Home auth={auth} />} />
                         <Route path="Act">
                             <Route index element={<Act auth={auth} />} />
-                            {/* <Route path="Atv" element={<Atv auth={auth} />} />
+                            <Route path="Atv" element={<Atv auth={auth} />} />
                             <Route path="Float" element={<Float auth={auth} />} />
                             <Route path="Night" element={<Night auth={auth} />} />
-                            <Route path="Upstream" element={<Upstream auth={auth} />} /> */}
+                            <Route path="Upstream" element={<Upstream auth={auth} />} />
                         </Route>
                         <Route path="Booking">
                             <Route index element={<Booking auth={auth} />} />
