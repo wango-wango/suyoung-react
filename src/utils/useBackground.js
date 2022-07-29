1d4757802772a1545efc3cabc749fc78b03857db
Jerry Wwang <doghost111@gmail.com>
2022-07-29 10:06:18 +0800

    useBackground 備註

--- src/utils/useBackground.js
+++ src/utils/useBackground.js
@@ -3,9 +3,11 @@ import { useState, useContext, createContext } from "react";
 const ThemeContext = createContext(null);
 
 export const ThemeProvider = ({ children }) => {
+    // background 預設背景圖狀態
     const [background, setBackground] = useState("bg1.svg");
 
     return (
+        // 更動整個頁面的背景狀態
         <ThemeContext.Provider value={{ background, setBackground }}>
             {children}
         </ThemeContext.Provider>
