2ab5870d016c8013c1dfa4e23d245663aedfd910
“terry19970422” <terry19970422@outlook.com>
2022-08-07 01:02:03 +0800

    新增套件與alert效果

--- src/pages/Carts/sub-pages/ShoppingCart/index.js
+++ src/pages/Carts/sub-pages/ShoppingCart/index.js
@@ -4,10 +4,23 @@ import RoomInfo from './Component/RoomInfo';
 import RoomLt from './Component/RoomLt';
 import RoomMo from './Component/RoomMo';
 import '../../styles/item.scss';
+const _ = require('lodash');
 
 function Index(props) {
-    const {setStep, cartQty, setCartQty, updateQty} = props;
-    const [sum, setSum] = useState();
+    const {setStep, roomItem, sum, setSum, orderItemsStr, HandleAlertBuy} = props;
+
+    // 處理訂單送出
+    const OrderSubmit = async (e) => {
+    if (!_.isEmpty(orderItemsStr)) {
+      // 購物車內有商品
+      setStep(2)
+    }
+    if (_.isEmpty(orderItemsStr)) {
+      // 如果購物車內沒有商品的話
+    HandleAlertBuy()
+    setStep(1)
+    }
+}
 
   return (
     <>
@@ -15,15 +28,12 @@ function Index(props) {
     <div className="room_info_and_web_component1">
     <div className="w100">
     <RoomInfo  
-            updateQty={updateQty}
-            cartQty={cartQty}
-            setCartQty={setCartQty}
             setSum={setSum}
             />
     </div>
        
-        <RoomLt setStep={setStep} sum={sum}/>
-        <RoomMo setStep={setStep} sum={sum}/>  
+        <RoomLt setStep={setStep} sum={sum} OrderSubmit={OrderSubmit}/>
+        <RoomMo setStep={setStep} sum={sum} OrderSubmit={OrderSubmit}/> 
     </div>
     </>
   )
