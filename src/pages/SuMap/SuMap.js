ee9cb55c61bd4f384b6e20b47933b3d6cc339eba
yenyu <diaoemily0830@gmail.com>
2022-08-08 03:29:11 +0800

    新增註冊畫面動畫

--- src/pages/SuMap/SuMap.js
+++ src/pages/SuMap/SuMap.js
@@ -1,28 +1,27 @@
 // import '../styles/footer.scss'
 // import '../styles/navbar.scss'
 // import '../css/su_map.css'
-import './SuMap.css'
-import React, {useEffect} from 'react';
+import "./SuMap.css";
+import React, { useEffect } from "react";
 import { useBackground } from "../../utils/useBackground";
 
 // import SuMapNav from "./components/SuMapNav";
 import SuMapContent from "./components/SuMapContent";
 // import SuMapFooter from "./components/SuMapFooter";
 
-function  SuMap(){
+function SuMap() {
     const { setBackground } = useBackground();
 
     useEffect(() => {
         setBackground("green_map.svg");
     }, []);
-    return(
+    return (
         <div>
-        {/* <SuMapNav/> */}
-        <SuMapContent/>
-        {/* <SuMapFooter/> */}
-        
+            {/* <SuMapNav/> */}
+            <SuMapContent />
+            {/* <SuMapFooter/> */}
         </div>
-    )
+    );
 }
 
-export default SuMap
+export default SuMap;
