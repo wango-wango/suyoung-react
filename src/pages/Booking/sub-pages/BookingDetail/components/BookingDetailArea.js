2ab394696a5ba887d3d796580b0b5eb628062340
Jerry Wwang <doghost111@gmail.com>
2022-08-09 16:27:02 +0800

    booking update 篩選功能完成 但仍須修bug

--- src/pages/Booking/sub-pages/BookingDetail/components/BookingDetailArea.js
+++ src/pages/Booking/sub-pages/BookingDetail/components/BookingDetailArea.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useBookingList } from "../../../../../utils/useBookingList";
 
 function BookingDetailArea(props) {
     const { roomList } = props;
-    console.log(roomList);
+    const { bookingList, setBookingList } = useBookingList();
+    console.log(bookingList);
 
     if (roomList.length === 0) return <></>;
 
@@ -13,27 +16,44 @@ function BookingDetailArea(props) {
                     <p>入住日期</p>
                 </div>
                 <div className="booking_date_in input_group">
-                    <input type="date" />
+                    <input
+                        type="text"
+                        readOnly
+                        placeholder={bookingList.startDate}
+                    />
                     <label htmlFor="">Date</label>
                 </div>
                 <div className="booking_date_title">
                     <p>離開日期</p>
                 </div>
                 <div className="booking_date_out input_group">
-                    <input type="date" />
+                    <input
+                        type="text"
+                        readOnly
+                        placeholder={bookingList.endDate}
+                    />
                     <label htmlFor="">Date</label>
                 </div>
                 <div className="booking_date_title">
                     <p>入住人數</p>
                 </div>
                 <div className="booking_person input_group">
-                    <input type="text" />
+                    <input
+                        type="text"
+                        readOnly
+                        placeholder={
+                            bookingList.adults +
+                            " 位大人 ｜ " +
+                            bookingList.kids +
+                            " 位小孩"
+                        }
+                    />
                     <label htmlFor="">Num</label>
                 </div>
             </div>
             <div className="room_Result">
                 <div className="room_title">
-                    {/* <h4>{roomList[0].room_name}</h4> */}
+                    <h4>{roomList[0].room_name}</h4>
                 </div>
                 <div className="room_result_type">
                     <div className="room_modify">
@@ -54,26 +74,32 @@ function BookingDetailArea(props) {
                         </div>
                         <div className="room_num">
                             <p>
-                                <span>2</span>晚
+                                <span>{bookingList.perNight}</span>晚
                             </p>
                         </div>
                     </div>
                     <div className="room_add_bed">
                         <div className="add_bed_name">
-                            <p>加床(TWD.100)</p>
+                            <p>加床(TWD.200)</p>
                         </div>
                         <div className="add_bed_num">
                             <p>
-                                <span>2</span>床
+                                <span>{bookingList.kids}</span>床
                             </p>
                         </div>
                     </div>
                     <div className="room_result_total">
                         <p>total:</p>
-                        <h5>{roomList[0].room_price * 2}</h5>
+                        <h5>
+                            {roomList[0].room_price * bookingList.perNight +
+                                bookingList.kids * 200}
+                        </h5>
                     </div>
                     <div className="room_card_button_area">
-                        <button className="room_card_button">button</button>
+                        <Link to="/shuyoung/Booking">
+                            <button className="room_card_button">返回</button>
+                        </Link>
+                        <button className="room_card_button">前往結賬</button>
                     </div>
                 </div>
             </div>
